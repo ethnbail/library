b1d8c693a018785e7b19e569f320541ee06ac15e
ethnbail <findeab@gmail.com>
2024-11-11 22:08:30 -0800

    updated

---
+++ Book.h
@@ -0,0 +1,21 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+using namespace std;
+
+class Book{
+    public: 
+        string title;
+        string author;
+        string serialNumber;
+        int publicationYear;
+        string genre;
+
+        Book(string t, string a, string s, int p, string g)
+        :title(t), author(a), serialNumber(s), publicationYear(p), genre(g){}
+        
+        void displayBooksInfo() const;
+};
+
+#endif
--- UserAccount.cpp
+++ UserAccount.cpp
@@ -5,38 +5,37 @@
 
 using namespace std;
 
-// Function to create a new account
+
 void createAccount() {
     string username, password;
-
     cout << "Enter a username: ";
     cin >> username;
     cout << "Enter a password: ";
     cin >> password;
 
-    // Open the file to check if the username already exists
-    ifstream infile("users.txt"); // Corrected file name to "users.txt"
+
+    ifstream infile("users.txt");// opens the users.txt
     string line;
 
-    // Check if the username already exists
+   
     while (getline(infile, line)) {
-        if (line.substr(0, line.find(',')) == username) {
+        if (line.substr(0, line.find(',')) == username) { // looks if the code has the username trying to be used
             cout << "This username already exists, try again" << endl;
-            infile.close();
+            infile.close(); 
             return;
         }
     }
     infile.close();
 
-    // If the username doesn't exist, append the new account to the file
-    ofstream outfile("users.txt", ios::app); // Corrected syntax for file output
+    
+    ofstream outfile("users.txt", ios::app); 
     outfile << username << "," << password << endl;
-    outfile.close();
+    outfile.close(); // adding the new usernameand password to the txt file to save it
 
     cout << "Account has been created" << endl;
 }
 
-// Function to sign in to an existing account
+
 bool signIn() {
     string username, password;
 
@@ -45,10 +44,10 @@ bool signIn() {
     cout << "Enter your password: ";
     cin >> password;
 
-    ifstream infile("users.txt"); // Corrected file name to "users.txt"
+    ifstream infile("users.txt"); 
     string line;
 
-    // Read the file and check for matching username and password
+  // finds the username & password that was given
     while (getline(infile, line)) {
         string storedUsername = line.substr(0, line.find(','));
         string storedPassword = line.substr(line.find(',') + 1);
@@ -56,11 +55,10 @@ bool signIn() {
         if (storedUsername == username && storedPassword == password) {
 
             infile.close();
-            return true; // Return true if the sign-in is successful
+            return true; 
         }
     }
 
     infile.close();
     cout << "Invalid username or password" << endl;
-    return false; // Return false if sign-in fails
-}
+    return false; 
